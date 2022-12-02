222324b16e34b92c8308864428367a3476b6a538
maximillian_sh <maximillian.sh23@gmail.com>
2022-12-02 16:15:39 +0200

    added player input service and character models

---
+++ Assets/CodeBase/Infrastructure/BootstrapInstaller.cs
@@ -0,0 +1,12 @@
+using Zenject;
+
+namespace CodeBase.Infrastructure
+{
+    public class ProjectInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+        
+        }
+    }
+}
